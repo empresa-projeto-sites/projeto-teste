9b9bd8c8dcc2498922e6be50b2c69d063b262e11
escola-reset <mrlancesfutebol@gmail.com>
2026-08-16 18:08:26 -0300

    Fix formatting issues in app.js

--- app.js
+++ app.js
@@ -4,3 +4,4 @@ if (menuButton && nav) {
   menuButton.addEventListener('click', () => nav.classList.toggle('open'));
   nav.querySelectorAll('a').forEach(link => link.addEventListener('click', () => nav.classList.remove('open')));
 }
+
