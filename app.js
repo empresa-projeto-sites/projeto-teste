c2974dc9efe58dd0466c9dee42d29ef9566fd667
escola-reset <mrlancesfutebol@gmail.com>
2026-08-16 18:04:07 -0300

    Add files via upload

---
+++ app.js
@@ -0,0 +1,6 @@
+const menuButton = document.querySelector('.menu-toggle');
+const nav = document.querySelector('header nav');
+if (menuButton && nav) {
+  menuButton.addEventListener('click', () => nav.classList.toggle('open'));
+  nav.querySelectorAll('a').forEach(link => link.addEventListener('click', () => nav.classList.remove('open')));
+}
